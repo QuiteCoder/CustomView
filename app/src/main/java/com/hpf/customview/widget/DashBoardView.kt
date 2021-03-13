ccadcd5231ad676ead072da8e3eb0e8e66140030
Allen <476657469@qq.com>
2021-03-13 08:31:49 +0800

    绘制仪表盘的刻度

--- app/src/main/java/com/hpf/customview/widget/DashBoardView.kt
+++ app/src/main/java/com/hpf/customview/widget/DashBoardView.kt
@@ -1,9 +1,9 @@
 package com.hpf.customview.widget
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Resources
-import android.graphics.Canvas
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.DisplayMetrics
 import android.util.Log
@@ -16,7 +16,7 @@ class DashBoardView(context: Context?, attrs: AttributeSet?) : View(context, att
     val TAG: String = "DashBoardView"
     var screenWidth: Int = 0
     var screenHeight: Int = 0
-    var paint : Paint = Paint().apply {
+    private var paint : Paint = Paint().apply {
         // 去除锯齿
         isAntiAlias = true
         //线宽
@@ -25,6 +25,7 @@ class DashBoardView(context: Context?, attrs: AttributeSet?) : View(context, att
         style = Paint.Style.STROKE
     }
 
+    private val scalePath = Path()
 
     init {
         val displayMetrics = Resources.getSystem().displayMetrics;
@@ -32,9 +33,16 @@ class DashBoardView(context: Context?, attrs: AttributeSet?) : View(context, att
         screenHeight =
             displayMetrics.heightPixels;// 表示屏幕的像素高度，单位是px（像素），获取到的高度不包含navigationBar和StatusBar
         Log.d(TAG, "init  screenWidth = $screenWidth  screenHeight = $screenHeight")
+        paint.strokeWidth = 5f
+        paint.isAntiAlias = true
+        paint.style = Paint.Style.STROKE
+        scalePath.addRect(0f, 0f, 10f, 15f, Path.Direction.CCW)
+        //paint.pathEffect = PathDashPathEffect(scalePath,50f,0f,PathDashPathEffect.Style.ROTATE)
     }
 
+    @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas) {
+        //绘制圆弧
         canvas.drawArc(screenWidth / 2f - RADIUS,
             screenHeight / 2 - RADIUS,
             screenWidth / 2 + RADIUS,
@@ -44,8 +52,17 @@ class DashBoardView(context: Context?, attrs: AttributeSet?) : View(context, att
             false,
             paint
         )
-        //canvas.drawLine(100f,100f,200f,200f,paint)
-
+        paint.pathEffect = PathDashPathEffect(scalePath,50f,0f,PathDashPathEffect.Style.ROTATE)
+        //绘制刻度
+        canvas.drawArc(screenWidth / 2f - RADIUS,
+            screenHeight / 2 - RADIUS,
+            screenWidth / 2 + RADIUS,
+            screenHeight / 2 + RADIUS,
+            90 + OPEN_ANGLE / 2f,
+            360 - OPEN_ANGLE,
+            false,
+            paint
+        )
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
