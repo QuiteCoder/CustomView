1b4df258c9695ff24af0df248fe5e9506aa7cbe0
Allen <476657469@qq.com>
2021-03-22 23:57:10 +0800

    EditTextView 添加副标题

--- app/src/main/java/com/hpf/customview/widget/MaterialEditText.kt
+++ app/src/main/java/com/hpf/customview/widget/MaterialEditText.kt
@@ -2,12 +2,32 @@ package com.hpf.customview.widget
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.widget.EditText
 import androidx.appcompat.widget.AppCompatEditText;
 
+const val TEXT_SIZE = 12f
+const val TEXT_MARGIN = 10f
 class MaterialEditText(context: Context, attrs: AttributeSet) : AppCompatEditText(context, attrs) {
+    private val mPaint = Paint().apply {
+        // 去除锯齿
+        isAntiAlias = true
+        //防抖动
+        isDither = true
+        textSize = TEXT_SIZE
+    }
+
+    init {
+        setPadding(paddingLeft,
+            (paddingTop+TEXT_SIZE+TEXT_MARGIN.toInt()).toInt(),paddingRight,paddingBottom)
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        //开始绘制悬浮副标题
+        if (!hint.isNullOrEmpty()) {
+            canvas.drawText(hint.toString(),10f,15f,paint)
+        }
     }
 }
