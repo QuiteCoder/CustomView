97776545716fd62ae5d1e9bcabe328205a5e52d9
Allen <476657469@qq.com>
2021-03-24 23:38:19 +0800

    EditTextView 加入文本升降特效

--- app/src/main/java/com/hpf/customview/widget/MaterialEditText.kt
+++ app/src/main/java/com/hpf/customview/widget/MaterialEditText.kt
@@ -11,6 +11,8 @@ import com.hpf.customview.dp
 
 val TEXT_SIZE = 10.dp
 val TEXT_MARGIN = 8.dp
+val FLOAT_TEXT_X = 5.dp
+val FLOAT_TEXT_Y = 20.dp
 
 class MaterialEditText(context: Context, attrs: AttributeSet) : AppCompatEditText(context, attrs) {
     private val mPaint = Paint().apply {
@@ -20,19 +22,19 @@ class MaterialEditText(context: Context, attrs: AttributeSet) : AppCompatEditTex
         isDither = true
         textSize = TEXT_SIZE
     }
-    private var floatTextShown:Boolean = false
+    private var floatTextShown:Boolean = true
     var floatTextFraction = 0f
         set(value) {
             field = value
             invalidate()
         }
     private val animator: ObjectAnimator by lazy {
-        Log.d("lazy","onTextChanged  canShowUpAnimation = " + floatTextShown)
+        Log.d("lazy","floatTextShown = " + floatTextShown)
         ObjectAnimator.ofFloat(this, "floatTextFraction", 0f, 1f)
     }
 
     init {
-        Log.d("init","onTextChanged  canShowUpAnimation = " + floatTextShown)
+        Log.d("init","floatTextShown = " + floatTextShown)
         setPadding(
             paddingLeft,
             (paddingTop + TEXT_SIZE + TEXT_MARGIN.toInt()).toInt(), paddingRight, paddingBottom
@@ -41,18 +43,19 @@ class MaterialEditText(context: Context, attrs: AttributeSet) : AppCompatEditTex
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        Log.d("onDraw","onTextChanged  canShowUpAnimation = " + floatTextShown)
+        Log.d("onDraw","floatTextShown = " + floatTextShown)
         //开始绘制悬浮副标题
         if (!hint.isNullOrEmpty()) {
             mPaint.alpha = (floatTextFraction*255).toInt()
-            canvas.drawText(hint.toString(), 5.dp, 20.dp, mPaint)
+            var dY = (1 - floatTextFraction) * FLOAT_TEXT_Y
+            canvas.drawText(hint.toString(), FLOAT_TEXT_X, FLOAT_TEXT_Y + dY, mPaint)
         }
     }
 
     override fun onTextChanged(text: CharSequence?, start: Int, lengthBefore: Int, lengthAfter: Int) {
         super.onTextChanged(text, start, lengthBefore, lengthAfter)
-        Log.d("onTextChanged","onTextChanged  canShowUpAnimation = " + floatTextShown)
-        if (!floatTextShown && !text.isNullOrEmpty()) {
+        Log.d("onTextChanged","floatTextShown = " + floatTextShown)
+        /*if (!floatTextShown && !text.isNullOrEmpty()) {
             //播放上升动画
             animator.start()
             floatTextShown = true
@@ -60,7 +63,7 @@ class MaterialEditText(context: Context, attrs: AttributeSet) : AppCompatEditTex
             //播放下降动画
             animator.reverse()
             floatTextShown = false
-        }
+        }*/
 
     }
 }
